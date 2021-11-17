94c1d6923fd98916b4b0226924ba668adcab172e
VolodumurSN <v.snaichuk@gmail.com>
2021-11-17 19:58:29 +0200

    Implement splash screen using react-native-splash-screen

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { Node } from 'react';
 import Home from './src/screens/Home/Home';
 import RoomItem from './src/components/RoomItem/RoomItem';
 import RoomsSearch from './src/screens/RoomsSearch/RoomsSearch';
 import LocationsSearch from './src/screens/LocationsSearch/LocationsSearch';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: () => Node = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return <RoomsSearch />;
 };
 
--- android/app/src/main/java/com/rn_airbnb/MainActivity.java
+++ android/app/src/main/java/com/rn_airbnb/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rn_airbnb;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "rn_airbnb";
+  }
+
+  /**
+   * Show SplashScreen
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 }
